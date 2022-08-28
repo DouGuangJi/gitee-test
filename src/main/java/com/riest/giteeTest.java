32db0aac5a4798c21d908b336e6e0c1feb686aad
DGJ <2575101192@qq.com>
2022-08-28 04:19:13 +0000

    update src/main/java/com/riest/giteeTest.java.
    Signed-off-by: DGJ <2575101192@qq.com>

--- src/main/java/com/riest/giteeTest.java
+++ src/main/java/com/riest/giteeTest.java
@@ -2,6 +2,6 @@ package com.riest;
 
 public class giteeTest {
     public static void main(String[] args) {
-        System.out.println("------");
+        System.out.println("------11111");
     }
 }
