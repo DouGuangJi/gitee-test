3e140245f592fd6dfae6c81c37cd3a348b19e96e
DouGuangJi <52559392+DouGuangJi@users.noreply.github.com>
2022-08-28 12:21:47 +0800

    Update

--- src/main/java/com/riest/giteeTest.java
+++ src/main/java/com/riest/giteeTest.java
@@ -2,6 +2,6 @@ package com.riest;
 
 public class giteeTest {
     public static void main(String[] args) {
-        System.out.println("------11111");
+        System.out.println("------1111122222");
     }
 }
